efd6c3417c7b09c0543c2ed9425e63c446f216c7
cindyweng18 <cindyweng18@gmail.com>
2024-09-11 18:23:08 -0400

    create signup pageand set up routes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Signup from './components/signup';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </Router>
+    
   </React.StrictMode>
 );
 
