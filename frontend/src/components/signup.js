fb072f0bd9c572b2aa81c649ad0d2ed8675bd032
Cindy <cindyweng18@gmail.com>
2024-09-26 20:55:11 -0400

    setup login request

--- frontend/src/components/signup.js
+++ frontend/src/components/signup.js
@@ -1,8 +1,16 @@
 import '../App.css';
-// import axios from 'axios';
+import axios from 'axios';
 import React from 'react';
 
 function Signup() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+    }
     return (
         <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
             <div class="sm:mx-auto sm:w-full sm:max-w-sm">
@@ -15,13 +23,13 @@ function Signup() {
                 <div>
                     <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
                     <div class="mt-2">
-                    <input id="email" name="email" type="email" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                    <input id="email" name="email" type="email" autocomplete="email" required value={email} onChange={(e) => setEmail(e.target.value)} class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                     </div>
                 </div>
 
                 <div>
                     <div class="flex items-center justify-between">
-                    <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
+                    <label id="password" name="password" type="password" autoComplete="current-password" for="password" value={password} onChange={(e) => setPassword(e.target.value)} class="block text-sm font-medium leading-6 text-gray-900">Password</label>
                     <div class="text-sm">
                         <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
                     </div>
