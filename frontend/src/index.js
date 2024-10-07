8a6a749e262569a4575b00d27be1d1c689877885
cindyweng18 <cindyweng18@gmail.com>
2024-10-07 17:30:37 -0400

    set up clerk

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,30 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Route, Routes } from 'react-router-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { AuthProvider } from './AuthContext';
-import './index.css';
+import ReactDOM from 'react-dom';
+import { ClerkProvider, SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import Signup from './components/signup';
-import Login from './components/login';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <AuthProvider>
-    <Router>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </Router>
-    </AuthProvider>
-  </React.StrictMode>
+ReactDOM.render(
+  <ClerkProvider frontendApi="your-clerk-frontend-api">
+    <SignedIn>
+      <UserButton />
+      <App />
+    </SignedIn>
+    <SignedOut>
+      <SignInButton />
+    </SignedOut>
+  </ClerkProvider>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
