adf215bca75ec2816d5cd14bc75b7e14f6c20b1b
cindyweng18 <cindyweng18@gmail.com>
2024-10-22 17:39:28 -0400

    minor fix

--- frontend/src/components/categories.js
+++ frontend/src/components/categories.js
@@ -41,11 +41,11 @@ const Categories = () => {
               <h1 className="text-4xl font-bold text-gray-900">Categories</h1>
     
               <div className="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-10">
-                {collections.map((category) => {
+                {collections.map(category => (
                   <a href={`/categories/${category.id}`} className="block max-w-sm p-20 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
                     <h5 className="mb-2 text-2xl text-center	font-bold tracking-tight text-gray-900 dark:text-white">{category.name}</h5>
                   </a>
-                })}
+                ))}
               </div>
             </div>
           </div>
