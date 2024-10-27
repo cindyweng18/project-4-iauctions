8c1e6e41d28a4c1e604574bbea32e06c1332b092
cindyweng18 <cindyweng18@gmail.com>
2024-10-26 21:07:25 -0400

    create comment section on listing page

--- frontend/src/components/listing.js
+++ frontend/src/components/listing.js
@@ -7,36 +7,36 @@ import { useEffect, useState } from "react";
 export default function Listing() {
     const navigate = useNavigate();
     const { isLoggedIn } = useAuth(); 
-    const [name, setName] = useState('');
-    const [price, setPrice] = useState('');
-    const [description, setDescription] = useState('');
+    const [name, setName] = useState('Lorem ipsum odor amet, consectetuer adipiscing elit.');
+    const [price, setPrice] = useState('$599.99');
+    const [description, setDescription] = useState('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.');
     const [comments, setComments] = useState([]);
     const [bids, setBids] = useState([]);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const id = this.props.match.params.id;
-            const token = localStorage.getItem('token');
-            if (isLoggedIn && token) {
-                try {
-                    const response = await axios.get(`http://localhost:8080/listings${id}`, {
-                        headers: {
-                            'Authorization': `Bearer ${token}`
-                        }
-                    });
-                    setName(response.data.data.title);
-                    setPrice(response.data.data.price);
-                    setDescription(response.data.data.description);
-                    setComments(response.data.data.comments);
-                    setBids(response.data.data.bids);
-                } catch (e) {
-                    console.error("Error fetching listing data: ", e);
-                }
-            } else {
-                navigate('/not-authorized');
-            }
+    // useEffect(() => {
+    //     const fetchData = async () => {
+    //         const id = this.props.match.params.id;
+    //         const token = localStorage.getItem('token');
+    //         if (isLoggedIn && token) {
+    //             try {
+    //                 const response = await axios.get(`http://localhost:8080/listings${id}`, {
+    //                     headers: {
+    //                         'Authorization': `Bearer ${token}`
+    //                     }
+    //                 });
+    //                 setName(response.data.data.title);
+    //                 setPrice(response.data.data.price);
+    //                 setDescription(response.data.data.description);
+    //                 setComments(response.data.data.comments);
+    //                 setBids(response.data.data.bids);
+    //             } catch (e) {
+    //                 console.error("Error fetching listing data: ", e);
+    //             }
+    //         } else {
+    //             navigate('/not-authorized');
+    //         }
 
-    }})
+    // }})
 
     return (
         <>
@@ -49,68 +49,11 @@ export default function Listing() {
                     <img class="w-full hidden dark:block" src="https://flowbite.s3.amazonaws.com/blocks/e-commerce/imac-front-dark.svg" alt="" />
                 </div>
 
-                <div class="mt-6 sm:mt-8 lg:mt-0">
+                <div class="mt-8 sm:mt-8 lg:mt-0">
                 <h1 class="text-xl font-semibold text-gray-900 sm:text-2xl dark:text-white"> {name} </h1>
                 <div class="mt-4 sm:items-center sm:gap-4 sm:flex">
                     <p class="text-2xl font-extrabold text-gray-900 sm:text-3xl dark:text-white"> {price} </p>
                 </div>
-
-                <div class="mt-6 sm:gap-4 sm:items-center sm:flex sm:mt-8">
-                    <a
-                    href="#"
-                    title=""
-                    class="flex items-center justify-center py-2.5 px-5 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-                    role="button"
-                    >
-                    <svg
-                        class="w-5 h-5 -ms-2 me-2"
-                        aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                    >
-                        <path
-                        stroke="currentColor"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="M12.01 6.001C6.5 1 1 8 5.782 13.001L12.011 20l6.23-7C23 8 17.5 1 12.01 6.002Z"
-                        />
-                    </svg>
-                    Add to favorites
-                    </a>
-
-                    <a
-                    href="#"
-                    title=""
-                    class="text-white mt-4 sm:mt-0 bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800 flex items-center justify-center"
-                    role="button"
-                    >
-                    <svg
-                        class="w-5 h-5 -ms-2 me-2"
-                        aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                    >
-                        <path
-                        stroke="currentColor"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="M4 4h1.5L8 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm.75-3H7.5M11 7H6.312M17 4v6m-3-3h6"
-                        />
-                    </svg>
-
-                    Add to cart
-                    </a>
-                </div>
-
-                <hr class="my-6 md:my-8 border-gray-200 dark:border-gray-800" />
 
                 <p class="mb-6 text-gray-500 dark:text-gray-400"> {description} </p>
 
@@ -118,6 +61,69 @@ export default function Listing() {
             </div>
             </div>
         </section>
+        <section class="bg-white dark:bg-gray-900 py-8 lg:py-16 antialiased">
+            <div class="max-w-5xl mx-auto px-4">
+                <div class="flex justify-between items-center mb-6">
+                    <h2 class="text-lg lg:text-2xl font-bold text-gray-900 dark:text-white">Discussion</h2>
+                </div>
+                <form class="mb-6">
+                    <div class="py-2 px-4 mb-4 bg-white rounded-lg rounded-t-lg border border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+                        <label for="comment" class="sr-only">Your comment</label>
+                        <textarea id="comment" rows="6"
+                            class="px-0 w-full text-sm text-gray-900 border-0 focus:ring-0 focus:outline-none dark:text-white dark:placeholder-gray-400 dark:bg-gray-800"
+                            placeholder="Write a comment..." required></textarea>
+                    </div>
+                    <button type="submit"
+                        class="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-primary-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800">
+                        Post comment
+                    </button>
+                </form>
+                {comments.map((comment) => (
+                  <article class="p-6 text-base bg-white rounded-lg dark:bg-gray-900">
+                    <footer class="flex justify-between items-center mb-2">
+                        <div class="flex items-center">
+                            <p class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white font-semibold"><img
+                                    class="mr-2 w-6 h-6 rounded-full"
+                                    src="https://flowbite.com/docs/images/people/profile-picture-2.jpg"
+                                    // TODO: Change user id to the name of user
+                                    alt="Michael Gough" />{comment.userId}</p>
+                            <p class="text-sm text-gray-600 dark:text-gray-400">
+                                {/* TODO: Change this time? */}
+                                <time pubdate datetime="2022-02-08" title="February 8th, 2022">{comment.createdAt}</time>
+                            </p>
+                        </div>
+                        <button id="dropdownComment1Button" data-dropdown-toggle="dropdownComment1"
+                            class="inline-flex items-center p-2 text-sm font-medium text-center text-gray-500 dark:text-gray-400 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-50 dark:bg-gray-900 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                            type="button">
+                            <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 3">
+                                <path d="M2 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm6.041 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM14 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Z"/>
+                            </svg>
+                            <span class="sr-only">Comment settings</span>
+                        </button>
+                        <div id="dropdownComment1"
+                            class="hidden z-10 w-36 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600">
+                            <ul class="py-1 text-sm text-gray-700 dark:text-gray-200"
+                                aria-labelledby="dropdownMenuIconHorizontalButton">
+                                <li>
+                                    <a href="#"
+                                        class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Edit</a>
+                                </li>
+                                <li>
+                                    <a href="#"
+                                        class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Remove</a>
+                                </li>
+                                <li>
+                                    <a href="#"
+                                        class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Report</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </footer>
+                    <p class="text-gray-500 dark:text-gray-400">{comment.description}</p>
+                </article>  
+                ))}
+            </div>
+            </section>
         <Footer />
         </>
     )
