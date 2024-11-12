eb5e1aa4c02ee6d8dc3340909218c465481e7b36
cindyweng18 <cindyweng18@gmail.com>
2024-11-11 19:50:11 -0500

    create modal to update email/password for users

--- frontend/src/components/account.js
+++ frontend/src/components/account.js
@@ -2,13 +2,14 @@ import React, { useEffect, useState } from "react";
 import Navbar from "./navbar";
 import Footer from "./footer";
 import { useNavigate } from "react-router-dom";
-import { useAuth } from '../utils';
+import { Dialog, DialogBackdrop, DialogPanel, DialogTitle } from '@headlessui/react'
 import axios from "axios";
 
 const Account = () => {
     const [email, setEmail] = useState('');
     const [listings, setListings] = useState([]);
     const navigate = useNavigate();
+    const [open, setOpen] = useState(true)
 
     useEffect(() => {
         const fetchUserData = async () => {
@@ -139,6 +140,96 @@ const Account = () => {
                 </div>
             </section>
             <Footer />
+
+            {/* Modal to edit/update email and password for user */}
+            <Dialog open={open} onClose={setOpen} className="relative z-10">
+                <DialogBackdrop
+                    transition
+                    className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in"
+                />
+
+                <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+                    <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                    <DialogPanel
+                        transition
+                        className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:w-full sm:max-w-lg data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95"
+                    >
+                        <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                        <div className="sm:flex sm:items-start">
+                            <div className="mx-auto flex h-12 w-12 shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                            
+                            </div>
+                            <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                            <DialogTitle as="h3" className="text-base font-semibold text-gray-900">
+                                Update Email/Password
+                            </DialogTitle>
+                            <div>
+                                <label htmlFor="email" className="block text-sm/6 font-medium text-gray-900">
+                                    Update Email
+                                </label>
+                                <div className="relative mt-2 rounded-md shadow-sm">
+                                    <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+                                    <span className="text-gray-500 sm:text-sm">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
+                                        </svg>
+
+                                    </span>
+                                    </div>
+                                    <input
+                                    id="new-email"
+                                    name="new-email"
+                                    type="text"
+                                    placeholder="your-email@example.com"
+                                    className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6"
+                                    />
+                                </div>
+                                </div>
+                                <div>
+                                <label htmlFor="password" className="block text-sm/6 font-medium text-gray-900">
+                                    Update Password
+                                </label>
+                                <div className="relative mt-2 rounded-md shadow-sm">
+                                    <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+                                    <span className="text-gray-500 sm:text-sm">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" />
+                                    </svg>
+                                    </span>
+                                    </div>
+                                    <input
+                                    id="new-password"
+                                    name="new-password"
+                                    type="text"
+                                    placeholder="New Password"
+                                    className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6"
+                                    />
+                                </div>
+                                </div>
+                            </div>
+                        </div>
+                        </div>
+                        <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                        <button
+                            type="button"
+                            onClick={() => setOpen(false)}
+                            className="inline-flex w-full justify-center rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
+                        >
+                            Update
+                        </button>
+                        <button
+                            type="button"
+                            data-autofocus
+                            onClick={() => setOpen(false)}
+                            className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                        >
+                            Cancel
+                        </button>
+                        </div>
+                    </DialogPanel>
+                    </div>
+                </div>
+                </Dialog>
         </>
 
 )};
